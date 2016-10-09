9d4711103fd4aa71042a42fafc5da8a0069ac2ec
Chris Sells <csells@sellsbrothers.com>
2016-10-09 13:41:53 -0700

    removed the minimalistic telnet client
    even it was more than I need. a simple TcpClient will do.

--- src/FibsLib/FibsLib.cs
+++ src/FibsLib/FibsLib.cs
@@ -1,27 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading.Tasks;
-using MinimalisticTelnet;
 
 namespace Fibs {
   // FIBS Client Protocol Detailed Specification: http://www.fibs.com/fibs_interface.html
   // Known hosts: fibs.com:4321 (default), tigergammon.com:4321
   public class FibsSession : IDisposable {
     static string FibsVersion = "1008";
-    TelnetConnection telnet;
+    TcpClient telnet = new TcpClient();
     CookieMonster monster = new CookieMonster();
+    byte[] readBuffer = new byte[4096];
 
     public FibsSession(string host = "fibs.com", int port = 4321) {
-      // from https://github.com/9swampy/Telnet/
-      telnet = new TelnetConnection(host, port);
-      if (!telnet.IsConnected) { throw new Exception($"cannot connect to {host} on {port}"); }
+      telnet.Client.Connect(host, port);
+      if (!telnet.Connected) { throw new Exception($"cannot connect to {host} on {port}"); }
     }
 
-    public bool IsConnected { get { return telnet.IsConnected; } }
+    public bool IsConnected { get { return telnet.Connected; } }
 
-    public async Task<IEnumerable<CookieMessage>> ReadMessagesAsync(int timeout = 5000) =>
-      Process(await telnet.ReadAsync(timeout));
+    //public async Task<IEnumerable<CookieMessage>> ReadMessagesAsync(int timeout = 5000) =>
+    //  Process(await ReadAsync(timeout));
 
     public async Task Login(string user, string pw) {
       await ExpectAsync(FibsCookie.FIBS_LoginPrompt);
@@ -35,13 +36,40 @@ namespace Fibs {
     IEnumerable<CookieMessage> Process(string s) =>
       s.Split(new string[] { "\r\n" }, StringSplitOptions.None).Select(l => monster.EatCookie(l)).ToArray();
 
-    public Task WriteLineAsync(string line) => telnet.WriteLineAsync(line);
+    public async Task WriteLineAsync(string line) {
+      await WriteAsync(line + "\n");
+    }
+
+    public async Task WriteAsync(string s) {
+      if (!telnet.Connected) { throw new Exception("not connected"); }
+      byte[] writeBuffer = ASCIIEncoding.ASCII.GetBytes(s);
+      await telnet.GetStream().WriteAsync(writeBuffer, 0, writeBuffer.Length);
+    }
+
+    public async Task<string> ReadAsync(int timeout = 5000) {
+      if (!telnet.Connected) { throw new Exception("not connected"); }
+
+      var start = DateTime.Now;
+      var span = new TimeSpan(0, 0, 0, 0, timeout);
+      var sb = new StringBuilder();
+
+      while (DateTime.Now - start < span) {
+        while (telnet.Available > 0) {
+          var count = telnet.GetStream().Read(readBuffer, 0, readBuffer.Length);
+          sb.Append(ASCIIEncoding.ASCII.GetString(readBuffer, 0, count));
+        }
+        await Task.Delay(1);
+      }
+
+      return sb.ToString();
+    }
 
     async Task ExpectAsync(FibsCookie cookie, int timeout = 5000) {
       var start = DateTime.Now;
       var span = new TimeSpan(0, 0, 0, 0, timeout);
       while (DateTime.Now - start < span) {
-        var s = await telnet.ReadAsync(1);
+        var s = await ReadAsync(1);
+        if (string.IsNullOrEmpty(s)) { continue; }
         var cookieMessages = Process(s);
         if (cookieMessages.Any(cm => cm.Cookie == cookie)) { return; }
       }
