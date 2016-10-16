ca7ddccd93df7409a2e441108600e5885a5fc23e
Chris Sells <csells@sellsbrothers.com>
2016-10-16 12:57:28 -0700

    simplify the code a bit

--- src/FibsProxy/SocketHandler.cs
+++ src/FibsProxy/SocketHandler.cs
@@ -29,13 +29,14 @@ namespace FibsProxy {
 
     async Task FibsLoop(WebSocket socket) {
       using (var fibs = new FibsSession()) {
+        var cancel = CancellationToken.None;
         var socketSegment = new ArraySegment<byte>(new byte[4096]);
 
         // wait for login
-        Task<WebSocketReceiveResult> socketReceiveTask = socket.ReceiveAsync(socketSegment, CancellationToken.None);
+        Task<WebSocketReceiveResult> socketReceiveTask = socket.ReceiveAsync(socketSegment, cancel);
         var socketInput = await socketReceiveTask;
         if (socketInput.MessageType != WebSocketMessageType.Text) {
-          await socket.CloseAsync(WebSocketCloseStatus.InvalidMessageType, "text messages only", CancellationToken.None);
+          await socket.CloseAsync(WebSocketCloseStatus.InvalidMessageType, "text messages only", cancel);
           return;
         }
 
@@ -44,24 +45,20 @@ namespace FibsProxy {
         var re = new Regex(@"login (?<user>[^ ]+) (?<password>[^ ]+)");
         var match = re.Match(s);
         if (!match.Success) {
-          await socket.CloseAsync(WebSocketCloseStatus.ProtocolError, "must login first", CancellationToken.None);
+          await socket.CloseAsync(WebSocketCloseStatus.ProtocolError, "must login first", cancel);
           return;
         }
-
-        // prepare to cancel ongoing tasks
-        var fibsCancel = new CancellationTokenSource();
-        var socketCancel = new CancellationTokenSource();
 
         // login to FIBS
         // TODO: support creating new users
         // TODO: support other servers besides fibs.com
         var user = match.Groups["user"].Value;
         var password = match.Groups["password"].Value;
-        Task<CookieMessage[]> fibsReadTask = fibs.Login(user, password, fibsCancel.Token);
+        Task<CookieMessage[]> fibsReadTask = fibs.Login(user, password);
         Debug.WriteLine($"FibsLoop: logging into FIBS for {user}");
 
         // get more socket input
-        socketReceiveTask = socket.ReceiveAsync(socketSegment, socketCancel.Token);
+        socketReceiveTask = socket.ReceiveAsync(socketSegment, cancel);
 
         while (socket.State == WebSocketState.Open) {
           var task = await Task.WhenAny(socketReceiveTask, fibsReadTask);
@@ -71,29 +68,29 @@ namespace FibsProxy {
             socketInput = await socketReceiveTask;
 
             if(socketInput.MessageType == WebSocketMessageType.Close) {
-              await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+              await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", cancel);
               continue;
             }
 
             if (socketInput.MessageType != WebSocketMessageType.Text) {
-              await socket.CloseAsync(WebSocketCloseStatus.InvalidMessageType, "text messages only", CancellationToken.None);
+              await socket.CloseAsync(WebSocketCloseStatus.InvalidMessageType, "text messages only", cancel);
               continue;
             }
 
             if (!socketInput.EndOfMessage) {
-              await socket.CloseAsync(WebSocketCloseStatus.MessageTooBig, "message too big", CancellationToken.None);
+              await socket.CloseAsync(WebSocketCloseStatus.MessageTooBig, "message too big", cancel);
               continue;
             }
 
             s = Encoding.UTF8.GetString(socketSegment.Array, 0, socketInput.Count);
             if (string.IsNullOrWhiteSpace(s)) {
-              await socket.CloseAsync(WebSocketCloseStatus.ProtocolError, "no empty messages", CancellationToken.None);
+              await socket.CloseAsync(WebSocketCloseStatus.ProtocolError, "no empty messages", cancel);
               continue;
             }
 
             // write socket input to FIBS and wait for more socket input
             await fibs.WriteLineAsync(s);
-            socketReceiveTask = socket.ReceiveAsync(socketSegment, socketCancel.Token);
+            socketReceiveTask = socket.ReceiveAsync(socketSegment, cancel);
           }
           else if (task.Equals(fibsReadTask)) {
             // read messages from FIBS
@@ -103,16 +100,16 @@ namespace FibsProxy {
               // write messages to socket
               var json = CookieMessageJsonConverter.ToJson(messages);
               var jsonBytes = Encoding.UTF8.GetBytes(json);
-              await socket.SendAsync(new ArraySegment<byte>(jsonBytes, 0, jsonBytes.Length), WebSocketMessageType.Text, true, CancellationToken.None);
+              await socket.SendAsync(new ArraySegment<byte>(jsonBytes, 0, jsonBytes.Length), WebSocketMessageType.Text, true, cancel);
 
               if (messages.Any(cm => cm.Cookie == FibsCookie.FIBS_Goodbye)) {
-                await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None);
+                await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", cancel);
                 break;
               }
             }
 
             // wait for more FIBS input
-            fibsReadTask = fibs.ReadMessagesAsync(fibsCancel.Token);
+            fibsReadTask = fibs.ReadMessagesAsync();
           }
           else {
             Debug.Assert(false, "Unknown task");
