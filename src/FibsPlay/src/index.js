585db95c2ba9697164b8b53792707853ae94fd71
Chris Sells <csells@sellsbrothers.com>
2017-07-09 13:44:45 -0700

    routes #fail

--- src/FibsPlay/src/index.js
+++ src/FibsPlay/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Routes from './routes';
+import Routes from './Routes';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
--- src/FibsPlay/src/routes.js
+++ src/FibsPlay/src/routes.js
@@ -1,35 +1,105 @@
 import React from 'react';
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect, withRouter } from 'react-router-dom'
 import { Navbar, Nav, NavItem } from 'react-bootstrap'
-import App from './App';
-import About from './About';
+import { LinkContainer } from 'react-router-bootstrap'
 import NotFound from './NotFound';
 
-// TODO: add auth flow from https://reacttraining.com/react-router/web/example/auth-workflow
-const Routes = (props) => (
-  <BrowserRouter>
-    <div>
-      <Navbar collapseOnSelect="true">
-        <Navbar.Header>
-          <Navbar.Brand>
-            Welcome to React App!
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav>
-            <NavItem><Link to="/">Home</Link></NavItem>
-            <NavItem><Link to="/about">About</Link></NavItem>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Switch>
-        <Route exact path="/" component={App} />
-        <Route path="/about" component={About} />
-        <Route component={NotFound} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
+const Welcome = () => <div>welcome</div>;
+
+const Login = withRouter(({props, history}) => <button onClick={()=>{
+    props.login("user", "pw");
+    props.history.push(props.state.from || "/logout");
+  }
+}>login</button>);
+
+const Register = withRouter(({props, history}) => <button onClick={()=>{
+      props.register("user", "pw", props.history);
+      props.history.push(props.state.from || "/logout");
+    }
+}>register</button>);
+
+const Logout = withRouter(({props, history}) => <button onClick={()=>{
+    props.logout(props.history);
+    props.history.push(props.state.from || "/login");
+  }
+}>logout</button>);
+
+const Watch = () => <div>watch</div>;
+const Play = () => <div>play</div>;
+const Chat = () => <div>chat</div>;
+
+class Routes extends React.Component {
+  state = {
+    user: null,
+  };
+
+  constantRouteLinks = [
+    { path: "/", text: "Welcome", component: Welcome },
+  ];
+
+  loggedOutRouteLinks = [
+    { path: "/login", text: "Login", component: () => <Login login={this.login} /> },
+    { path: "/register", text: "Register", component: () => <Register register={this.register} /> },
+  ];
+
+  loggedInRouteLinks = [
+    { path: "/logout", text: "Logout", component: () => <Logout logout={this.logout} /> },
+    { path: "/watch", text: "Watch", component: Watch },
+    { path: "/play", text: "Play", component: Play },
+    { path: "/chat", text: "Chat", component: Chat },
+  ];
+
+  login = (user, pw) => {
+    this.setState({user: user});
+  };
+
+  register = (user, pw) => {
+    this.setState({user: user});
+    history.push(props.location.state.from || "/logout");
+  };
+
+  logout = () => {
+    this.setState({user: null});
+    history.push(props.location.state.from || "/login");
+  };
+
+  render() {
+    let loggedIn = !!this.state.user;
+    let routeLinks = constantRouteLinks.slice().concat(loggedIn ? loggedInRouteLinks : loggedOutRouteLinks);
+
+    return (
+      <Router>
+        <div>
+          <Navbar collapseOnSelect>
+            <Navbar.Header>
+              <Navbar.Brand>Play FIBS!</Navbar.Brand>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav>
+                {routeLinks.map(rl => (<LinkContainer key={rl.path} to={rl.path}><NavItem>{rl.text}</NavItem></LinkContainer>))}
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <Switch>
+            {this.constantRouteLinks.map(rl => (<Route exact key={rl.path} path={rl.path} component={rl.component} />))}
+            {this.loggedOutRouteLinks // login, register, ...
+              .map(rl => !loggedIn
+                ? (<Route key={rl.path} path={rl.path} component={rl.component} />)
+                : (<Redirect key={rl.path} from={rl.path} to={{pathname: "/login", state: {from: this.props.location}}} />)
+            )}
+            {this.loggedInRouteLinks // play, watch, chat
+              .map(rl => loggedIn
+                ? (<Route key={rl.path} path={rl.path} component={rl.component} />)
+                : (<Redirect key={rl.path} from={rl.path} to={{pathname: "/logout", state: {from: this.props.location}}} />)
+            )}
+            <Route component={NotFound} />
+          </Switch>
+          <div>user: {this.state.user}</div>
+        </div>
+      </Router>
+    );
+  }
+};
 
 export default Routes;
