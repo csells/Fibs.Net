75b0e482a76390ed0a00b053b16982e109558da2
Chris Sells <csells@sellsbrothers.com>
2017-11-18 16:28:27 -0800

    very basics of the login form

--- src/FibsPlay/src/components/Login.vue
+++ src/FibsPlay/src/components/Login.vue
@@ -1,19 +1,36 @@
 <template>
-  <div class="login">
-    {{ msg }}
-  </div>
-</template>
+  <form v-on:submit="login">
+      <div><span>user: <input v-model="user" /></span></div>
+      <div><span>password: <input v-model="pw" type="password" /></span></div>
+      <div><input type="submit" value="login" /></div>
+      <div>status: {{status}}</div>
+      <div>error: {{error}}</div>
+      <div>last login: {{lastLogin}}</div>
+      <div>last host: {{lastHost}}</div>
+    </form>
+  </template>
 
 <script>
 export default {
-  name: "login",
   data() {
     return {
-      msg: "login (duh)"
+      user: "",
+      pw: "",
+      status: "ok",
+      error: "none",
+      lastLogin: "unknown",
+      lastHost: "unknown"
     };
+  },
+  methods: {
+    login: function(e) {
+      e.preventDefault();
+      alert(`user= '${this.user}', pw=${this.pw}`);
+    }
   }
 };
 </script>
 
 <style scoped>
+
 </style>
