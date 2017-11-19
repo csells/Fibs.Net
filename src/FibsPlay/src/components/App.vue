68a788373621a9290f1e29c48417d662da4d0a92
Chris Sells <csells@sellsbrothers.com>
2017-11-18 17:55:53 -0800

    made the login form handle validation

--- src/FibsPlay/src/components/App.vue
+++ src/FibsPlay/src/components/App.vue
@@ -9,12 +9,6 @@
   </div>
 </template>
 
-<script>
-export default {
-  name: "app"
-};
-</script>
-
 <style scoped>
 body {
   margin: 0;
--- src/FibsPlay/src/components/Login.vue
+++ src/FibsPlay/src/components/Login.vue
@@ -1,36 +1,63 @@
 <template>
   <form v-on:submit="login">
-      <div><span>user: <input v-model="user" /></span></div>
-      <div><span>password: <input v-model="pw" type="password" /></span></div>
-      <div><input type="submit" value="login" /></div>
-      <div>status: {{status}}</div>
-      <div>error: {{error}}</div>
-      <div>last login: {{lastLogin}}</div>
-      <div>last host: {{lastHost}}</div>
+      <div><span>user: <input v-model="user" :disabled=inputDisabled /></span></div>
+      <div><span>password: <input v-model="pass" type="password" :disabled=inputDisabled /></span></div>
+      <div><input type="submit" v-model="submitName" :disabled=submitDisabled /></div>
+      <div>status: {{session.status}}</div>
+      <div>error: {{session.error}}</div>
+      <div>last login: {{session.lastLogin}}</div>
+      <div>last host: {{session.lastHost}}</div>
     </form>
   </template>
 
 <script>
+let session = {
+  status: "closed",
+  error: null,
+  lastLogin: null,
+  lastHost: null,
+  login: function(user, pass) {
+    this.status = "opened";
+  },
+  logout: function() {
+    this.status = "closed";
+  }
+};
+
 export default {
   data() {
     return {
-      user: "",
-      pw: "",
-      status: "ok",
-      error: "none",
-      lastLogin: "unknown",
-      lastHost: "unknown"
+      user: "dotnetcli", // HACK: devmode
+      pass: "dotnetcli", // HACK: devmode
+      session
     };
   },
+
+  computed: {
+    submitName: function() {
+      return this.session.status === "closed" ? "Login" : "Logout";
+    },
+
+    submitDisabled: function() {
+      return (
+        this.session.status === "closed" && (this.user === "" || this.pass === "")
+      );
+    },
+
+    inputDisabled: function() {
+      return this.session.status === "opened";
+    }
+  },
+
   methods: {
     login: function(e) {
       e.preventDefault();
-      alert(`user= '${this.user}', pw=${this.pw}`);
+      if (session.status === "closed") {
+        session.login(this.user, this.pass);
+      } else {
+        session.logout();
+      }
     }
   }
 };
 </script>
-
-<style scoped>
-
-</style>
